70d82fec78586dab959e457efb31a8519d3ccda3
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-02-01 10:08:08 +0545

    ufrontend

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PagesController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -16,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[PagesController::class,'index'])->name('home');
 
 Route::middleware('auth')->group(function(){
     //Category
